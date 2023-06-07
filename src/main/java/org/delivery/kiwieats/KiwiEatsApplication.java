5d59d3c63534d9263d76caef57d2794b346bd230
Muhammad.PC <muhammadshamaeem@gmail.com>
2023-06-07 22:40:27 +0200

    initial commit

---
+++ src/main/java/org/delivery/kiwieats/KiwiEatsApplication.java
@@ -0,0 +1,11 @@
+package org.delivery.kiwieats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KiwiEatsApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(KiwiEatsApplication.class, args);
+	}
+}
---
+++ src/test/java/org/delivery/kiwieats/KiwieatsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.delivery.kiwieats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KiwieatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
