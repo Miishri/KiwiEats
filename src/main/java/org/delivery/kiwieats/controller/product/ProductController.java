6f2d2e2b4dcbb6ae56dcfaf88c8141c3504d9ac6
Muhammad.PC <muhammadshamaeem@gmail.com>
2023-06-09 21:16:44 +0200

    add empty product controller

---
+++ src/main/java/org/delivery/kiwieats/controller/product/ProductController.java
@@ -0,0 +1,11 @@
+package org.delivery.kiwieats.controller.product;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RequiredArgsConstructor
+@RestController
+public class ProductController {
+}
