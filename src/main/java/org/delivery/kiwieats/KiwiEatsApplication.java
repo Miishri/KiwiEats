2f1d4ed704aafb031f54bf245bc9103aa88a8edb
Muhammad.PC <muhammadshamaeem@gmail.com>
2023-06-09 19:38:00 +0200

    add exclude DataSourceAutoConfig.class (IMP)

--- src/main/java/org/delivery/kiwieats/KiwiEatsApplication.java
+++ src/main/java/org/delivery/kiwieats/KiwiEatsApplication.java
@@ -2,8 +2,9 @@ package org.delivery.kiwieats;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class KiwiEatsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(KiwiEatsApplication.class, args);
