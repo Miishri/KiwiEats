09cff51cc04df52ec8efc682431399d4c5e21fba
Muhammad.PC <muhammadshamaeem@gmail.com>
2023-06-09 21:17:23 +0200

    add empty rider controller

---
+++ src/main/java/org/delivery/kiwieats/controller/rider/RiderController.java
@@ -0,0 +1,11 @@
+package org.delivery.kiwieats.controller.rider;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RequiredArgsConstructor
+@RestController
+public class RiderController {
+}
