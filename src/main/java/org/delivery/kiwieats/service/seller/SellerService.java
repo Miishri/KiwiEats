a15b677046dc936f64ea7c6e3a1e92c661dccdfb
Muhammad.PC <muhammadshamaeem@gmail.com>
2023-06-09 21:10:34 +0200

    add interface methods seller

--- src/main/java/org/delivery/kiwieats/service/seller/SellerService.java
+++ src/main/java/org/delivery/kiwieats/service/seller/SellerService.java
@@ -1,4 +1,18 @@
 package org.delivery.kiwieats.service.seller;
 
+import org.delivery.kiwieats.model.seller.SellerDTO;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 public interface SellerService {
+    List<SellerDTO> listSellers();
+    Optional<SellerDTO> getSellerById();
+
+    SellerDTO saveSeller(SellerDTO sellerDTO);
+
+    Optional<SellerDTO> updateSellerById(UUID sellerId, SellerDTO sellerDTO);
+
+    Boolean deleteSellerById(UUID sellerId);
 }
