b1e4982fd2a625f8d49b01ae8b4f1ca92ca4a8e0
Muhammad.PC <muhammadshamaeem@gmail.com>
2023-06-09 21:09:52 +0200

    add interface methods

--- src/main/java/org/delivery/kiwieats/service/customer/CustomerService.java
+++ src/main/java/org/delivery/kiwieats/service/customer/CustomerService.java
@@ -1,4 +1,19 @@
 package org.delivery.kiwieats.service.customer;
 
+import org.delivery.kiwieats.model.customer.CustomerDTO;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 public interface CustomerService {
+    List<CustomerDTO> listCustomers();
+    Optional<CustomerDTO> getCustomerById();
+
+    CustomerDTO saveCustomer(CustomerDTO customerDTO);
+
+    Optional<CustomerDTO> updateCustomerById(UUID customerId, CustomerDTO customerDTO);
+
+    Boolean deleteCustomerById(UUID customerId);
+
 }
