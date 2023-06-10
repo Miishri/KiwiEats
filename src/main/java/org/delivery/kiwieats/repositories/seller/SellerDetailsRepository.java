6db1810170867cc1a29e5007c83ec557c6988b1f
Muhammad.PC <muhammadshamaeem@gmail.com>
2023-06-10 17:11:37 +0200

    extend sellerRepo with jpa

---
+++ src/main/java/org/delivery/kiwieats/repositories/seller/SellerDetailsRepository.java
@@ -0,0 +1,7 @@
+package org.delivery.kiwieats.repositories.seller;
+
+import org.delivery.kiwieats.entities.seller.SellerDetails;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SellerDetailsRepository extends JpaRepository<SellerDetails, Long> {
+}
