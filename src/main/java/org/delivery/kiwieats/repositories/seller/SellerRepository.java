ed28f1b5a2d8167295a99e437ce50868d32aea8f
Muhammad.PC <muhammadshamaeem@gmail.com>
2023-06-10 17:11:56 +0200

    extend sellerRepo with jpa

--- src/main/java/org/delivery/kiwieats/repositories/seller/SellerRepository.java
+++ src/main/java/org/delivery/kiwieats/repositories/seller/SellerRepository.java
@@ -1,4 +1,9 @@
 package org.delivery.kiwieats.repositories.seller;
 
-public interface SellerRepository {
+import org.delivery.kiwieats.entities.seller.Seller;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface SellerRepository extends JpaRepository<Seller, UUID> {
 }
