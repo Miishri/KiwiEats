52427f2052b2998f26251ec03618b91769c425e0
Muhammad.PC <muhammadshamaeem@gmail.com>
2023-06-12 02:01:51 +0200

    fix annotation

--- src/main/java/org/delivery/kiwieats/KiwiEatsApplication.java
+++ src/main/java/org/delivery/kiwieats/KiwiEatsApplication.java
@@ -1,10 +1,9 @@
 package org.delivery.kiwieats;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@EnableAutoConfiguration
 public class KiwiEatsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(KiwiEatsApplication.class, args);
