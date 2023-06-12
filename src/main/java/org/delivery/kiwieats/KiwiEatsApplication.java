4a0021445d97fc2e399388037e28bf091f8f9b60
Muhammad.PC <muhammadshamaeem@gmail.com>
2023-06-12 23:11:05 +0200

    annotate with @springbootapp

--- src/main/java/org/delivery/kiwieats/KiwiEatsApplication.java
+++ src/main/java/org/delivery/kiwieats/KiwiEatsApplication.java
@@ -1,11 +1,9 @@
 package org.delivery.kiwieats;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@EnableAutoConfiguration
-@ComponentScan
+@SpringBootApplication
 public class KiwiEatsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(KiwiEatsApplication.class, args);
