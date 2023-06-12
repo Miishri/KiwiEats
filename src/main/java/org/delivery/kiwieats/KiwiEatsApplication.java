2590920220f6ace5faeef05dc2212fbb15fccd4e
Muhammad.PC <muhammadshamaeem@gmail.com>
2023-06-12 18:42:38 +0200

    add component scan

--- src/main/java/org/delivery/kiwieats/KiwiEatsApplication.java
+++ src/main/java/org/delivery/kiwieats/KiwiEatsApplication.java
@@ -2,8 +2,10 @@ package org.delivery.kiwieats;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
 @EnableAutoConfiguration
+@ComponentScan
 public class KiwiEatsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(KiwiEatsApplication.class, args);
