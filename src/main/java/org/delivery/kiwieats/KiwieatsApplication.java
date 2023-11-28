b86d57e9137ddd66c9eaee08decc4656630cf8ad
mishi <mishi@gmail.com>
2023-11-28 12:18:32 +0100

    reset project

--- src/main/java/org/delivery/kiwieats/KiwieatsApplication.java
+++ src/main/java/org/delivery/kiwieats/KiwieatsApplication.java
@@ -4,8 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class KiwiEatsApplication {
+public class KiwieatsApplication {
+
 	public static void main(String[] args) {
-		SpringApplication.run(KiwiEatsApplication.class, args);
+		SpringApplication.run(KiwieatsApplication.class, args);
 	}
+
 }
--- src/main/java/org/delivery/kiwieats/entities/product/ProductType.java
+++
@@ -1,5 +0,0 @@
-package org.delivery.kiwieats.entities.product;
-
-public enum ProductType {
-    GAMING, TECHNOLOGY, GYM, GIFT, BEAUTY, FOOD, CLOTHING
-}
