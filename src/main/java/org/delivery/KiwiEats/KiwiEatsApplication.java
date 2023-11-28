68b47f845b53e0a0e6d55d64f3aef1f70398d39c
mishi <mishi@gmail.com>
2023-11-28 13:04:26 +0100

    fix name conflicts

--- src/main/java/org/delivery/KiwiEats/KiwiEatsApplication.java
+++ src/main/java/org/delivery/KiwiEats/KiwiEatsApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class KiwiatsApplication {
+public class KiwiEatsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(KiwieatsApplication.class, args);
+		SpringApplication.run(KiwiEatsApplication.class, args);
 	}
 
 }
