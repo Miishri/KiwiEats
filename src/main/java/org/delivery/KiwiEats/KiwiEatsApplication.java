e12bafa81fcf1d95e3b65f24e7f5cb683a8fbe96
mishi <mishi@gmail.com>
2023-12-12 14:18:57 +0100

    add hateoas

--- src/main/java/org/delivery/KiwiEats/KiwiEatsApplication.java
+++ src/main/java/org/delivery/KiwiEats/KiwiEatsApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class KiwiEatsApplication {
-
-	public static void main(String[] args) {
+	public static void main(String... args) {
 		SpringApplication.run(KiwiEatsApplication.class, args);
 	}
-
 }
