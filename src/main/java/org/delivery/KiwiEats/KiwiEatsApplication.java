82a2543a270693a5d3a3188dc0f4f136d0575862
Kiwi <muhammadshamaeem@example.com>
2023-12-23 02:11:08 +0100

    add spotify google format plugin with goal & test format

--- src/main/java/org/delivery/KiwiEats/KiwiEatsApplication.java
+++ src/main/java/org/delivery/KiwiEats/KiwiEatsApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class KiwiEatsApplication {
-	public static void main(String... args) {
-		SpringApplication.run(KiwiEatsApplication.class, args);
-	}
+  public static void main(String... args) {
+    SpringApplication.run(KiwiEatsApplication.class, args);
+  }
 }
--- src/main/java/org/delivery/KiwiEats/services/ProductService.java
+++ src/main/java/org/delivery/KiwiEats/services/ProductService.java
@@ -1,15 +1,17 @@
 package org.delivery.KiwiEats.services;
 
-import org.delivery.KiwiEats.entities.Product;
-
 import java.util.List;
 import java.util.Optional;
+import org.delivery.KiwiEats.entities.Product;
 
 public interface ProductService {
-    Optional<Product> getProductById(Long id);
-    List<Product> getAllProducts();
-    Product createProduct(Product product);
-    Optional<Product> updateProductById(Long id, Product product);
-    Product deleteProductById(Long id);
+  Optional<Product> getProductById(Long id);
+
+  List<Product> getAllProducts();
+
+  Product createProduct(Product product);
+
+  Optional<Product> updateProductById(Long id, Product product);
+
+  Product deleteProductById(Long id);
 }
- 
