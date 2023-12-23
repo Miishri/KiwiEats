e5a126a4a5ca234559c1f6ea07fdcbd168495547
Kiwi <muhammadshamaeem@example.com>
2023-12-23 02:04:21 +0100

    add findbyid & findall & update product methods

--- src/main/java/org/delivery/KiwiEats/services/ProductService.java
+++ src/main/java/org/delivery/KiwiEats/services/ProductService.java
@@ -2,7 +2,14 @@ package org.delivery.KiwiEats.services;
 
 import org.delivery.KiwiEats.entities.Product;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ProductService {
+    Optional<Product> getProductById(Long id);
+    List<Product> getAllProducts();
     Product createProduct(Product product);
+    Optional<Product> updateProductById(Long id, Product product);
     Product deleteProductById(Long id);
 }
+ 
--- src/main/java/org/delivery/KiwiEats/services/ProductServiceImpl.java
+++ src/main/java/org/delivery/KiwiEats/services/ProductServiceImpl.java
@@ -1,24 +1,59 @@
 package org.delivery.KiwiEats.services;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.delivery.KiwiEats.entities.Product;
 import org.delivery.KiwiEats.repositories.ProductRepository;
 import org.hibernate.service.spi.ServiceException;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicReference;
+
 @Slf4j
 @Service
+@RequiredArgsConstructor
 public class ProductServiceImpl implements ProductService {
+
     private final ProductRepository productRepository;
 
-    public ProductServiceImpl(ProductRepository productRepository) {
-        this.productRepository = productRepository;
+    @Override
+    public Optional<Product> getProductById(Long id) {
+        log.debug("SERVICE - Get product by ID - Product ID: " + id + " - SERVICE");
+        return productRepository.findById(id);
+    }
+
+    @Override
+    public List<Product> getAllProducts() {
+        log.debug("SERVICE - Get all products - SERVICE ");
+        return productRepository.findAll();
     }
 
     @Override
     public Product createProduct(Product product) {
         log.debug("SERVICE--Create new product from controller--SERVICE");
         return productRepository.save(product);
+    }
+
+    @Override
+    public Optional<Product> updateProductById(Long id, Product product) {
+        log.debug("SERVICE - Update product by existing product and ID: " + id + " - SERVICE");
+        AtomicReference<Optional<Product>> productReference = new AtomicReference<>();
+
+        productRepository.findById(id).ifPresentOrElse(productFound -> {
+            productFound.setProductImage(product.getProductImage());
+            productFound.setProductName(product.getProductName());
+            productFound.setCategory(product.getCategory());
+            productFound.setCreationDate(product.getCreationDate());
+
+            productReference.set(Optional.of(productRepository.save(productFound)));
+
+        }, () -> {
+            productReference.set(Optional.empty());
+        });
+
+        return productReference.get();
     }
 
     @Override
