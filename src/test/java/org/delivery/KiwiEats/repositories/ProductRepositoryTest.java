149196d156beeaf67c3a0b9c36711ba4e7090b9e
Skanda Sharma <skandasharma55@gmail.com>
2023-12-24 22:19:46 +0530

    Added repository test for create and delete (#16)
    * Added repository test for create and delete
    
    * Modified tests for better readability
    
    * Optimised readability
    
    * Modified getProductListFromRepo function

--- src/test/java/org/delivery/KiwiEats/repositories/ProductRepositoryTest.java
+++ src/test/java/org/delivery/KiwiEats/repositories/ProductRepositoryTest.java
@@ -2,9 +2,16 @@ package org.delivery.KiwiEats.repositories;
 
 import org.delivery.KiwiEats.entities.Category;
 import org.delivery.KiwiEats.entities.Product;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
 public class ProductRepositoryTest {
@@ -20,5 +27,28 @@ public class ProductRepositoryTest {
             .category(Category.FRUIT)
             .build();
     setupRepository.save(product);
+  }
+
+  @Test
+  @Transactional
+  public void testCreateProduct() {
+    assertThat(getProductListFromRepo().get(0).getProductName()).isEqualTo("Apple");
+  }
+
+  @Test
+  @Transactional
+  public void testDeleteProductById() {
+    Long id = getProductListFromRepo().get(0).getId();
+    productRepository.deleteById(id);
+    assertThat(getProductListFromRepo()).isEmpty();
+  }
+
+  @AfterAll
+  public static void tearDown(@Autowired ProductRepository setupRespository) {
+    setupRespository.flush();
+  }
+
+  private List<Product> getProductListFromRepo() {
+    return productRepository.findAll();
   }
 }
