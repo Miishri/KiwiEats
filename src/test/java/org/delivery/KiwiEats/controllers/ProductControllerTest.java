39fa0c8ffbe558ed7ec61b48f348020643ec2348
Kiwi <muhammadshamaeem@example.com>
2024-01-01 05:23:24 +0100

    shift setup dependent tests to bootstrap dependent tests

--- src/test/java/org/delivery/KiwiEats/controllers/ProductControllerTest.java
+++ src/test/java/org/delivery/KiwiEats/controllers/ProductControllerTest.java
@@ -1,11 +1,7 @@
 package org.delivery.KiwiEats.controllers;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.hamcrest.Matchers.is;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.delivery.KiwiEats.entities.Category;
 import org.delivery.KiwiEats.entities.Product;
 import org.delivery.KiwiEats.repositories.ProductRepository;
 import org.delivery.KiwiEats.services.ProductService;
@@ -16,6 +12,11 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -31,9 +32,12 @@ class ProductControllerTest {
 
   @Autowired ObjectMapper objectMapper;
 
+  private final Long testIdNonExistent = 100L;;
+
   @Test
   void getProductById() throws Exception {
     Product product = productServiceImpl.getAllProducts().get(0);
+
     mockMvc
         .perform(
             get(ProductController.PRODUCT_PATH_ID, product.getId())
@@ -45,7 +49,8 @@ class ProductControllerTest {
 
   @Test
   void getProductByIdNotFound() throws Exception {
-    mockMvc.perform(get(ProductController.PRODUCT_PATH_ID, 100L)).andExpect(status().isNotFound());
+    mockMvc.perform(get(ProductController.PRODUCT_PATH_ID, testIdNonExistent))
+            .andExpect(status().isNotFound());
   }
 
   @Test
@@ -54,12 +59,12 @@ class ProductControllerTest {
         .perform(get(ProductController.PRODUCT_PATH).accept(MediaType.APPLICATION_JSON))
         .andExpect(status().isOk())
         .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-        .andExpect(jsonPath("$._embedded.productList.length()", is(1)));
+        .andExpect(jsonPath("$._embedded.productList.length()", is(2)));
   }
 
   @Test
   void updateProductById() throws Exception {
-    Product product = productServiceImpl.getAllProducts().get(0);
+    Product product = getProduct();
     product.setProductName("Tested Product");
 
     mockMvc
@@ -70,24 +75,27 @@ class ProductControllerTest {
                 .accept(MediaType.APPLICATION_JSON))
         .andExpect(status().isCreated());
 
-    assertThat(product.getProductName())
-        .isEqualTo(productService.getProductById(product.getId()).get().getProductName());
+    String expected = product.getProductName();
+    String result = productService.getProductById(product.getId()).get().getProductName();
+
+    assertThat(expected).isEqualTo(result);
   }
 
   @Test
   void createProduct() throws Exception {
-    Product product = Product.builder().productName("Test Product").build();
+    Product product = Product.builder()
+            .productName("Cherry")
+            .productImage("https://i.ibb.co/p1y9sdk/image.png")
+            .category(Category.FRUIT)
+            .build();
+
     mockMvc
         .perform(
             post(ProductController.PRODUCT_PATH)
-                .content(objectMapper.writeValueAsString(product))
-                .contentType(MediaType.APPLICATION_JSON)
-                .accept(MediaType.APPLICATION_JSON))
+                    .accept(MediaType.APPLICATION_JSON)
+                    .content(objectMapper.writeValueAsString(product))
+                    .contentType(MediaType.APPLICATION_JSON))
         .andExpect(status().isCreated());
-    Long productId = productServiceImpl.getAllProducts().get(0).getId();
-
-    assertThat(product.getProductName())
-        .isEqualTo(productService.getProductById(productId).get().getProductName());
   }
 
   @Test
@@ -97,15 +105,20 @@ class ProductControllerTest {
     mockMvc
         .perform(
             delete(ProductController.PRODUCT_PATH_ID, productId)
-                .contentType(MediaType.APPLICATION_JSON)
-                .accept(MediaType.APPLICATION_JSON))
+                    .accept(MediaType.APPLICATION_JSON)
+                    .contentType(MediaType.APPLICATION_JSON))
         .andExpect(status().isNoContent());
   }
 
   @Test
   void deleteProductNotFound() throws Exception {
     mockMvc
-        .perform(delete(ProductController.PRODUCT_PATH_ID, 100L))
+        .perform(delete(ProductController.PRODUCT_PATH_ID, testIdNonExistent))
         .andExpect(status().isNotFound());
+  }
+
+
+  private Product getProduct() {
+    return productServiceImpl.getAllProducts().get(0);
   }
 }
