1eeb03f3cd425120e24581bf5a5763e4b7a4a4de
Skanda Sharma <skandasharma55@gmail.com>
2024-01-02 22:03:28 +0530

    Add DTO for Product entity (#45)
    * Add DTO for Product entity
    
    * Addressed PR comments

---
+++ src/main/java/org/delivery/KiwiEats/models/ProductDTO.java
@@ -0,0 +1,40 @@
+package org.delivery.KiwiEats.models;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Builder;
+import lombok.Data;
+import org.delivery.KiwiEats.entities.Category;
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
+
+import java.sql.Timestamp;
+
+@Data
+@Builder
+public class ProductDTO {
+    private Long id;
+
+    @NotNull
+    @NotBlank(message = "Product Name cannot be empty")
+    @Length(max=200)
+    private String productName;
+
+    @NotNull
+    @URL(protocol = "https",message = "Image is not in the right format!")
+    private String productImage;
+
+    @NotBlank(message = "Category cannot be empty")
+    private Category category;
+
+    @NotNull
+    @NotBlank(message = "Price cannot be empty")
+    @Size(min = 1, max = 1000)
+    private Double price;
+
+    private Timestamp creationDate;
+
+    @NotNull
+    private Timestamp lastUpdatedDate;
+}
