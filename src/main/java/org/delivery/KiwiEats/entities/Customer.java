9eaba3a8b557b63ec6a99367ad81d67f8d6f7a20
Skanda <skandasharma55@gmail.com>
2024-01-03 22:49:23 +0530

    Added customer entity

---
+++ src/main/java/org/delivery/KiwiEats/entities/Customer.java
@@ -0,0 +1,43 @@
+package org.delivery.KiwiEats.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.sql.Timestamp;
+
+@Entity
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "Customer")
+public class Customer {
+
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  @Id
+  @Column(name = "customer_id")
+  private Long customerId;
+
+  @Column(name = "first_name")
+  private String firstName;
+
+  @Column(name = "last_name")
+  private String lastName;
+
+  @Column(name = "email_id")
+  private String emailId;
+
+  @Column(name = "password")
+  private String password;
+
+  @CreationTimestamp
+  @Column(name = "customer_creation_date")
+  private Timestamp creationDate;
+
+  @UpdateTimestamp
+  @Column(name = "last_updated_date")
+  private Timestamp lastUpdatedDate;
+}
