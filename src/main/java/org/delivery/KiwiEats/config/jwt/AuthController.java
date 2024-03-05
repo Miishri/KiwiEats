85e9d0f7d9bae2cd5bb274e15f2c72a9c42ae680
Miishri SKOL <muhammadshamaeem@elev.ga.ntig.se>
2024-03-05 12:52:24 +0100

    expore jwt generation endpoint

---
+++ src/main/java/org/delivery/KiwiEats/config/jwt/AuthController.java
@@ -0,0 +1,17 @@
+package org.delivery.KiwiEats.config.jwt;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class AuthController {
+    private final TokenService tokenService;
+
+    @PostMapping("/generate-token")
+    public String token(Authentication authentication) {
+        return tokenService.generateToken(authentication);
+    }
+}
